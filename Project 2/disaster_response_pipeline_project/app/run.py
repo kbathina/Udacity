6e952652ccba9d95e316949649fbc0a3e1cbd257
Krishna C. Bathina <kbathina@umich.edu>
2022-01-09 18:36:59 -0600

    finished commenting

--- Project 2/disaster_response_pipeline_project/app/run.py
+++ Project 2/disaster_response_pipeline_project/app/run.py
@@ -10,6 +10,8 @@ import nltk
 from nltk.tag import pos_tag
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+
+# save all possible POS tags
 pos_tags = ['CC','CD','DT','EX','FW','IN','JJ','JJR',
  'JJS','LS','MD','NN','NNP','NNPS','NNS',
  'PDT','POS','PRP','PRP$','RB','RBR','RBS',
@@ -27,36 +29,73 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 class POS_Counter(BaseEstimator, TransformerMixin):
+    '''
+    class that counts parts of speech tags using the nltk pos
+    '''
 
     def pos_dict_maker(self):
+        '''
+        initializes a count dictionary with all possible pos tags from nltk
+        input: None
+        output: count dict
+        '''
+        # initialize empty dictionary
         pos_dict = {}
+
+        # add each tag to the dictionary with a count of 0
         for pos in pos_tags:
             pos_dict[pos] = 0
         return pos_dict
 
     def fit(self, X, y=None):
+        '''
+        empty fit function
+        '''
         return self
 
     def transform(self, X):
+        '''
+        builds parts of speech count dictionar as features for ML
+        input: collection of text (str)
+        output: df of parts of speech counts
+        '''
+        # intitialize list to hold count dictionaries for each text
         X_tagged = []
+
+        # iterate through each text
         for text in X:
+            # initialize a count dictionary
             pos_dict = self.pos_dict_maker()
-            counts = Counter([pos for word,pos in pos_tag(word_tokenize(text))])            
+            
+            # count number of each parts of speech using nltk pos_tag
+            counts = Counter([pos for word,pos in pos_tag(word_tokenize(text))])     
+            # update count dictionary       
             for k,v in counts.items():
                 if k.isalnum():
                     pos_dict[k] = v
-                    
+            # append to list
             X_tagged.append(pos_dict)
 
+        # convert to pandas and replace NA with 0 and then return
         return pd.DataFrame(X_tagged).fillna(0)
 
 def tokenize(text):
+    '''
+    tokenizing function
+    input: text 
+    output: cleaned text
+    '''
+    # create tokens by tokenizing words
     tokens = word_tokenize(text)
+    # initalize lemmatizer
     lemmatizer = WordNetLemmatizer()
 
+    # initializa list to store tokens
     clean_tokens = []
     for tok in tokens:
+        # lemmatize, lower case, and remove white strips
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        # append cleaned token to the list
         clean_tokens.append(clean_tok)
 
     return clean_tokens
@@ -74,26 +113,36 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/index')
 def index():
     
-    # extract data needed for visuals
+    ## extract data needed for visuals
+    # drop non-label columbs
+    # get counts of number of labels per tweet
     label_counts = df.drop(['message','original','genre', 'VADER'], axis=1).sum(axis = 1).value_counts()
+    # stores number of labels per tweet
     label_names = list(label_counts.index)
 
+    ## plot histogram of sentiment
+    # make bins of size 0.05 from -1 (most sad) to 1 (most happy)
     bins = np.linspace(-1,1,21)
+    # split sentiment into the bins and groupby each bin
     sentiment = df['VADER'].groupby(pd.cut(df['VADER'], bins=bins)).size()
+    # convert index from object to string
     sentiment.index = sentiment.index.map(str)
+    # store variables for plot
     sentiment_x = sentiment.index
     sentiment_y = sentiment.values
     
-    # create visuals
+    # create visuals for home page
     graphs = [
         {
+            # bar graph of number of labels per tweet
             'data': [
                 Bar(
                     x=label_names,
                     y=label_counts
                 )
             ],
-
+            
+            # add labels to graph
             'layout': {
                 'title': 'Number of Labels per Tweet',
                 'yaxis': {
@@ -104,14 +153,15 @@ def index():
                 }
             }
         },
-        {
+        {   # histogram of sentiment
             'data': [
                 Bar(
                     x=sentiment_x,
                     y=sentiment_y
                 )
             ],
-
+            
+            # add labels to graph
             'layout': {
                 'title': 'VADER Sentiment',
                 'yaxis': {
--- Project 2/disaster_response_pipeline_project/data/process_data.py
+++ Project 2/disaster_response_pipeline_project/data/process_data.py
@@ -3,50 +3,95 @@ import pandas as pd
 from sqlalchemy import create_engine
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
+
 nltk.download('vader_lexicon')
 sid = SentimentIntensityAnalyzer()
 
+
 def load_data(messages_filepath, categories_filepath):
+    '''
+    loads data from user inputted location
+    input: 2 str locations
+    output: dataframe consisting of both datasets
+    '''
+    # read in message
     messages = pd.read_csv(messages_filepath, sep = ',', index_col = 'id')
+    # read in categories
     categories = pd.read_csv(categories_filepath, sep = ',', index_col = 'id')
+    # concat data together
     df = messages.join(categories)
     return df
 
 
 def clean_data(df):
+    '''
+    cleans dataframe
+    input: dataframe
+    output: cleaned dataframe
+    '''
+
+    # split data into separate columns
     categories = df['categories'].str.split(';',expand = True)
+    # get list of labels
     category_colnames = categories.iloc[0].apply(lambda x: x.split('-')[0])
+    # set columns names as labels
     categories.columns = category_colnames
-    categories = categories.applymap(lambda x:int(x.split('-')[1])).replace(2,0)
+    # convert label data into 1 or 0
+    categories = categories.applymap(lambda x:int(x.split('-')[1]))
+    # replace 2 with 0 - according to data page
+    categories = categories.replace(2,0)
+    # delete old categories column
     del df['categories']
+    # join newly made label data with original data
     df = df.join(categories)
+    # remove duplicatees
     df = df[~df.duplicated()]
+    # apply vader sentiment analysis
     df['VADER'] = df['message'].apply(lambda x: sid.polarity_scores(x)['compound'])
     return df
 
 
 def save_data(df, database_filename):
+    '''
+    saves data to a sqlite database
+    input: df to save and filepath
+    output: None
+    '''
     engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('disaster_tweets', engine, index=False)  
+    pass
 
 
 def main():
+    '''
+    runs the etl functions
+    '''
+    
+    # makes sure the user gives 3 inputs
     if len(sys.argv) == 4:
 
+        # save inputs as variables
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
+
+        # save concatenated data
         df = load_data(messages_filepath, categories_filepath)
 
+        # clean data
         print('Cleaning data...')
         df = clean_data(df)
         
+        # save into sqlite
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
     
+
+    # print error message in case there is not enough inputs
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
